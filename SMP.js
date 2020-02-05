18d684e507ee4643d7f10f12c1af93aee21527d0
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-02-05 14:40:54 +0300

    1

--- SMP.js
+++ SMP.js
@@ -1,28 +1,48 @@
+/*jshint esversion: 6 */
 class SMP {
 
-	constructor(TEMPLATE = null) {
+	constructor(TEMPLATE = null, R = null) {
+		this.R = R;
 		this.TEMPLATE = TEMPLATE;
 		this.init();
+		this.FadeDurability = 500;
 	}
 	setTemp(TEMPLATE) {
 		this.TEMPLATE = TEMPLATE;
+		this.init();
+		return this;
 	}
 	init() {
+		if (this.body) {
+			this.body.remove();
+		}
 		$('.SMP-wrap').remove();
 		this.body = $(`<div class="SMP-wrap">`).appendTo('body');
+		return this;
 	}
-	open(R) {
-		this.TEMPLATE = this.strstr(this.TEMPLATE, R);
-		this.modal = $(`<div class="SMP-modal">`).appendTo(this.body);
-		this.modal.html("");
-		this.ativity = $(this.TEMPLATE).appendTo(this.modal);
-		this.body.fadeIn(500);
+	open(R = null) {
+		if (R) {
+			this.R = R;
+		}
+		if (!this.ativity) {
+			this.HTML = this.strstr(this.TEMPLATE, this.R);
+			this.modal = $(`<div class="SMP-modal">`).appendTo(this.body);
+			this.modal.html("");
+			this.ativity = $(this.HTML).appendTo(this.modal);
+		}
+		this.body.fadeIn(this.FadeDurability);
+		return this;
 	}
 	close() {
-
+		this.body.fadeOut(this.FadeDurability);
+		return this;
 	}
-	change() {
-
+	change(R = null) {
+		if (R) {
+			this.R = R;
+		}
+		this.HTML = this.strstr(this.TEMPLATE, R);
+		return this;
 	}
 
 	strstr(haystack, needle) {	// Find first occurrence of a string
