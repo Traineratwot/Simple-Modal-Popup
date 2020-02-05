92de85a0d5fa2f72a31065cecdef5e601e353328
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-02-05 17:07:25 +0300

    5

--- SMP.js
+++ SMP.js
@@ -9,50 +9,75 @@ class SMP {
 		this.R = R;
 		this.OPTION = OPTION;
 		this.TEMPLATE = TEMPLATE;
-		this.init();
+		this.cls = this.makeRandomClass(5);
 		this.FadeDurability = 500;
+		this.events = {};
+		this.init();
 	}
 	setTemp(TEMPLATE) {
 		this.TEMPLATE = TEMPLATE;
-		this.init();
+		this.gen();
 		return this;
 	}
 	init() {
+		var self = this;
 		if (this.body) {
 			this.body.remove();
 		}
-		this.body = $(`<div class="SMP-wrap">`).appendTo('body');
-
+		if (this.OPTION.background) {
+			this.body = $(`<div class="${this.cls} SMP-wrap SMP-wrap-custom">`).appendTo('body');
+			this.body.css(this.OPTION.background.css)
+		} else {
+			this.body = $(`<div class="${this.cls} SMP-wrap SMP-wrap-defult">`).appendTo('body');
+		}
+		this.body.on("click touch", (event) => {
+			self.events.body = event
+			if ($(event.target).index() == self.body.index()) {
+				self.close()
+			}
+		})
+		this.gen()
 	}
 	/**
 	 * @param  {} R=null
 	 * @param  {} OPTION={}
 	 */
 	gen() {
-		if (!this.ativity) {
-			this.HTML = this.strstr(this.TEMPLATE, this.R);
-			if (Object.keys(this.OPTION).length) {
-				this.modal = $(`<div class="SMP-modal SMP-custom">`).appendTo(this.body);
-				if (this.OPTION.css) {
-					this.modal.css(this.OPTION.css)
-				}
-				if (this.OPTION.offset) {
-					this.modal.offset(this.OPTION.offset)
-				}
-				// if (this.OPTION.css) {
-				// 	this.modal.css(this.OPTION.css)
-				// }
-
-			} else {
-				this.modal = $(`<div class="SMP-modal SMP-center">`).appendTo(this.body);
+		var self = this;
+		this.HTML = this.strstr(this.TEMPLATE.pattern, this.R);
+		if (Object.keys(this.OPTION.popup).length) {
+			this.modal = $(`<div class="SMP-modal SMP-modal-custom">`).appendTo(this.body);
+			if (this.OPTION.popup.css) {
+				this.modal.css(this.OPTION.popup.css)
 			}
-			this.modal.html("");
-			this.ativity = $(this.HTML).appendTo(this.modal);
+			if (this.OPTION.popup.offset) {
+				this.modal.offset(this.OPTION.popup.offset)
+			} else {
+				this.modal.addClass("SMP-modal-center");
+			}
+			// if (this.OPTION.css) {
+			// 	this.modal.css(this.OPTION.css)
+			// }
+		} else {
+			this.modal = $(`<div class="SMP-modal SMP-modal-center">`).appendTo(this.body);
+		}
+		this.modal.html("");
+		this.ativity = $(this.HTML).appendTo(this.modal);
+
+		for (const key in this.TEMPLATE.action) {
+			if (this.TEMPLATE.action.hasOwnProperty(key)) {
+				const e = this.TEMPLATE.action[key];
+				$(`.${this.cls} ${key}`).on(e.event, function (event) {
+					self.events[key] = (event);
+					return e.function(event, self, self.AnyValue)
+				})
+			}
 		}
 		return this;
 	}
-	open(R = null, OPTION = {}) {
+	open(AnyValue = null, R = null, OPTION = {}) {
 		var regen = false;
+		this.AnyValue = AnyValue;
 		if (R) {
 			this.R = R;
 			regen = true;
@@ -74,11 +99,18 @@ class SMP {
 	/**
 	 * @param  {} R=null
 	 */
-	change(R = null) {
+	setData(R) {
 		if (R) {
 			this.R = R;
 		}
-		this.HTML = this.strstr(this.TEMPLATE, R);
+		this.gen()
+		return this;
+	}
+	setOption(OPTION) {
+		if (OPTION) {
+			this.R = OPTION;
+		}
+		this.gen()
 		return this;
 	}
 
@@ -93,5 +125,15 @@ class SMP {
 			}
 		}
 		return haystack;
+	}
+
+	makeRandomClass(length) {
+		var result = '';
+		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		var charactersLength = characters.length;
+		for (var i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
 	}
 }
